aca97b110df290ef118d112adf88ffb711cdce3e
Scifiknux <scifiknux@yahoo.com>
2024-01-05 00:01:06 -0700

    Added test scenes.
    I'm learning! Two scenes, and two sets of texts.

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
 window.onload = function() {
-    var game = new Phaser.Game();
+    var config = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: 0x507D2A,
+        scene: [Scene1, Scene2]
+    }
+    var game = new Phaser.Game(config);
 }
