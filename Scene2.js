66fc76eebab9328dbaa7d30f21d41167e117d42d
Scifiknux <scifiknux@yahoo.com>
2024-01-08 19:45:31 -0700

    Starting animations.

--- Scene2.js
+++ Scene2.js
@@ -8,9 +8,16 @@ class Scene2 extends Phaser.Scene {
         this.background.scale = 8;
         this.background.setOrigin(0,0);
 
-        this.ship1 = this.add.image(this.game.config.width/2 - 50, this.game.config.height/2, "ship");
-        this.ship2 = this.add.image(this.game.config.width/2, this.game.config.height/2, "ship2");
-        this.ship3 = this.add.image(this.game.config.width/2 + 50, this.game.config.height/2, "ship3");
+        this.ship1 = this.add.sprite(this.game.config.width/2 - 50, this.game.config.height/2, "ship");
+        this.ship2 = this.add.sprite(this.game.config.width/2, this.game.config.height/2, "ship2");
+        this.ship3 = this.add.sprite(this.game.config.width/2 + 50, this.game.config.height/2, "ship3");
+
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1
+        });
 
         this.add.text(20,20, "Playing game", {
             font: "25px Arial",
